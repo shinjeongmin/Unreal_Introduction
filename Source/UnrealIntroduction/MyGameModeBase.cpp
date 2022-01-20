52f255d66dbced20c659de0a8f66b8718ab73c97
Shin <sin37minduke@naver.com>
2022-01-20 14:43:15 +0900

    Create blueprint class

--- Source/UnrealIntroduction/MyGameModeBase.cpp
+++ Source/UnrealIntroduction/MyGameModeBase.cpp
@@ -8,4 +8,12 @@ AMyGameModeBase::AMyGameModeBase()
 {
 	// GameMode의 DefaultPawnClass값을 변경
 	DefaultPawnClass = AMyCharacter::StaticClass();
+
+	// 블루프린트 클래스 넣기
+	static ConstructorHelpers::FClassFinder<ACharacter> BP_Char(TEXT("Blueprint'/Game/Blueprints/BP_MyCharacter.BP_MyCharacter_C'"));
+
+	if (BP_Char.Succeeded())
+	{
+		DefaultPawnClass = BP_Char.Class;
+	}
 }
